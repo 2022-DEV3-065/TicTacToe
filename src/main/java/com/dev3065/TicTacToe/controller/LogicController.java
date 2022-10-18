313f8286e041ea638757e9fa9a4f5bad407edfb1
2022-DEV3-065 <2022dev3065@gmail.com>
2022-10-18 18:18:38 +0200

    Fixed string equals bug.

--- src/main/java/com/dev3065/TicTacToe/controller/LogicController.java
+++ src/main/java/com/dev3065/TicTacToe/controller/LogicController.java
@@ -7,12 +7,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;xx
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RestController;
 
 import static org.slf4j.LoggerFactory.getLogger;
-
 
 @RestController
 public class LogicController {
