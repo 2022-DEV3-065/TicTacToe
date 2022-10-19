25bb7d1308831a724bc8dc02170b1ba918f88411
2022-DEV3-065 <2022dev3065@gmail.com>
2022-10-19 12:19:50 +0200

    Refactored winning condition to be more readable.

--- src/main/java/com/dev3065/TicTacToe/Helper.java
+++ src/main/java/com/dev3065/TicTacToe/Helper.java
@@ -1,6 +1,7 @@
 package com.dev3065.TicTacToe;
 
 import com.dev3065.TicTacToe.domain.Player;
+
 import static com.dev3065.TicTacToe.domain.Player.*;
 
 import java.util.List;
@@ -18,24 +19,17 @@ public class Helper {
 
     public static boolean winCondition(List<Player> state) {
         state = state.subList(0, 9);
-        if (state.get(0).equals(state.get(1)) && state.get(1).equals(state.get(2)) && !state.get(0).equals(EMPTY)) {
-            return true;
-        } else if (state.get(3).equals(state.get(4)) && state.get(4).equals(state.get(5)) && !state.get(3).equals(EMPTY)) {
-            return true;
-        } else if (state.get(6).equals(state.get(7)) && state.get(7).equals(state.get(8)) && !state.get(6).equals(EMPTY)) {
-            return true;
-        } else if (state.get(0).equals(state.get(3)) && state.get(3).equals(state.get(6)) && !state.get(0).equals(EMPTY)) {
-            return true;
-        } else if (state.get(1).equals(state.get(4)) && state.get(4).equals(state.get(7)) && !state.get(1).equals(EMPTY)) {
-            return true;
-        } else if (state.get(2).equals(state.get(5)) && state.get(5).equals(state.get(8)) && !state.get(2).equals(EMPTY)) {
-            return true;
-        } else if (state.get(0).equals(state.get(4)) && state.get(4).equals(state.get(8)) && !state.get(0).equals(EMPTY)) {
-            return true;
-        } else if (state.get(2).equals(state.get(4)) && state.get(4).equals(state.get(6)) && !state.get(2).equals(EMPTY)) {
-            return true;
-        } else {
-            return false;
-        }
+        return isWinningLine(0, 1, 2, state) ||
+                isWinningLine(3, 4, 5, state) ||
+                isWinningLine(6, 7, 8, state) ||
+                isWinningLine(0, 3, 6, state) ||
+                isWinningLine(1, 4, 7, state) ||
+                isWinningLine(2, 5, 8, state) ||
+                isWinningLine(0, 4, 8, state) ||
+                isWinningLine(2, 4, 6, state);
+    }
+
+    private static boolean isWinningLine(int i, int i1, int i2, List<Player> state) {
+        return state.get(i).equals(state.get(i1)) && state.get(i1).equals(state.get(i2)) && !state.get(i).equals(EMPTY);
     }
 }
